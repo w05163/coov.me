869b706cede2064aaaea865a7baa024d89ee4744
w05163 <979350927@qq.com>
2025-07-25 22:16:26 +0800

    fix: test

--- index.js
+++ index.js
@@ -14,30 +14,35 @@ import authorization from './middleware/auth';
 import socketInit from './websocket';
 
 const app = new Koa();
-DBInit();// 数据库初始化
+DBInit(); // 数据库初始化
 app.use(cors({
-	...config.cors,
-	origin(ctx) {
-    if (config.cors.origin.includes(ctx.header.origin)) {
-      return ctx.header.origin;
-    }
-    return false;
-  }
-}));// 跨域配置
+    ...config.cors,
+    origin(ctx) {
+      if (config.cors.origin.includes(ctx.header.origin)) {
+        return ctx.header.origin;
+      }
+      return false;
+    },
+  })); // 跨域配置
 app.use(staticMiddleware(config.staticPath)); // 设置静态资源目录
 app.use(bodyParser());
 app.use(authorization);
-app.use(router.routes());// 路由
+app.use(router.routes()); // 路由
 
 app.use((ctx) => {
-	ctx.body = '没有匹配到任何内容';
+  ctx.body = '没有匹配到任何内容';
 });
 
-const server = https.createServer({
-	key: fs.readFileSync('./bin/csr/privatekey.key'),
-	cert: fs.readFileSync('./bin/csr/certificate.crt')
-}, app.callback()).listen(config.port);
+const server = https
+  .createServer(
+    {
+      key: fs.readFileSync('./bin/csr/privatekey.key'),
+      cert: fs.readFileSync('./bin/csr/certificate.crt'),
+    },
+    app.callback()
+  )
+  .listen(config.port);
 
-socketInit(server);// 启动websocket服务器
+socketInit(server); // 启动websocket服务器1
 
 console.log('服务器已启动');
